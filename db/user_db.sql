0d4172c726cbada66ebc4e591084553ccc0cd8f0
Eric <ericsunjaya1105@gmail.com>
2023-06-25 17:48:30 +0700

    update db

--- db/user_db.sql
+++ db/user_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 23 Jun 2023 pada 16.18
+-- Waktu pembuatan: 25 Jun 2023 pada 12.47
 -- Versi server: 10.4.19-MariaDB
 -- Versi PHP: 8.0.7
 
@@ -24,6 +24,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `produk`
+--
+
+CREATE TABLE `produk` (
+  `bpom` char(30) NOT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  `serving` varchar(20) DEFAULT NULL,
+  `energy` varchar(20) DEFAULT NULL,
+  `lemak` varchar(20) DEFAULT NULL,
+  `lemakjenuh` varchar(20) DEFAULT NULL,
+  `protein` varchar(20) DEFAULT NULL,
+  `karbo` varchar(20) DEFAULT NULL,
+  `gula` varchar(20) DEFAULT NULL,
+  `garam` varchar(20) DEFAULT NULL,
+  `caffeine` int(2) DEFAULT NULL,
+  `seafood` int(2) DEFAULT NULL,
+  `dairy` int(2) DEFAULT NULL,
+  `egg` int(2) DEFAULT NULL,
+  `peanut` int(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `users`
 --
 
@@ -40,8 +64,25 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `caffeine`, `seafood`, `dairy`, `egg`, `peanut`) VALUES
+(1, 'admin', 'admin@gmail.com', '21232f297a57a5a743894a0e4a801fc3', 1, 1, 1, 1, 1),
+(2, 'testing', 'testing@gmail.com', 'ae2b1fca515949e5d54fb22b8ed95575', 0, 0, 0, 0, 0),
+(5, 'a', 'a@gmail.com', '0cc175b9c0f1b6a831c399e269772661', 1, 0, 1, 0, 0),
+(7, 'b', 'b@gmail.com', '92eb5ffee6ae2fec3ad71c777531578f', 1, 0, 0, 0, 0),
+(8, 'c', 'c@gmail.com', '4a8a08f09d37b73795649038408b5f33', 0, 1, 0, 0, 1);
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`bpom`);
 
 --
 -- Indeks untuk tabel `users`
@@ -57,7 +98,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
